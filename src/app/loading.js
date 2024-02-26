599483c74a9966a0811b70a3bfb133e1b88c5fbd
Arzena Akter <arzenaakter@gmail.com>
2024-02-26 11:59:59 +0600

    add loading page

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div>
+             <h1 className=' mt-20 font-bold text-4xl'>Loading ....</h1>
+        </div>
+    );
+};
+
+export default LoadingPage;
